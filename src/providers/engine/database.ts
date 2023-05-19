745f380e04b0522d71c6585d0b2d4b00db8fec72
ada0l <andreika.varfolomeev@yandex.ru>
2023-05-19 16:57:45 +1000

    API requests rewritten using filterByFormula

--- src/providers/engine/database.ts
+++ src/providers/engine/database.ts
@@ -18,25 +18,36 @@ export default class Database {
         }
     }
 
+    static generateFilterForProperties(areas, beds, minPrice, price, properties = []) {
+        // The api has a limitation on working with arrays. There are no functions
+        // for arrays as contain or in. Therefore, the search for the number is
+        // performed by the string with the number treated with commas
+        const propertiesFormula = `NOT(SEARCH(CONCATENATE(",", {Номер} ,","), ',${properties},'))`;
+        const res = `
+        AND(
+            ${(properties.length) ? propertiesFormula : "TRUE()"},
+            {Модерация},
+            SEARCH({Район}, '${areas}'),
+            SEARCH({Количество спален}, '${beds}'),
+            {Цена долларов в месяц} >= ${minPrice},
+            {Цена долларов в месяц} <= ${price}
+        )
+        `
+        return res;
+    }
+
     static async findProperties(areas, beds, minPrice, price) {
         const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY });
         try {
             const records = await airtable
                 .base(process.env.AIRTABLE_BASE_ID)
                 .table(process.env.AIRTABLE_PROPERTIES_TABLE_ID)
-                .select()
+                .select({
+                    filterByFormula: this.generateFilterForProperties(
+                        areas, beds, minPrice, price)
+                })
                 .all();
-            return records.filter((record) => {
-                let filtered = record.get('Модерация') &&
-                    areas.includes(record.get('Район')) &&
-                    beds.includes(record.get('Количество спален')) &&
-                    price >= record.get('Цена долларов в месяц');
-                if (minPrice != null) {
-                  filtered = filtered &&
-                    minPrice <= record.get('Цена долларов в месяц');
-                }
-                return filtered;
-            });
+            return records;
         } catch (exception) {
             console.error(`issue detected ...\n${exception}`);
         }
@@ -48,20 +59,12 @@ export default class Database {
             const records = await airtable
                 .base(process.env.AIRTABLE_BASE_ID)
                 .table(process.env.AIRTABLE_PROPERTIES_TABLE_ID)
-                .select()
+                .select({
+                    filterByFormula: this.generateFilterForProperties(
+                        areas, beds, minPrice, price, properties)
+                })
                 .all();
-            return records.filter((record) => {
-                let filtered = !properties.includes(`${record.get('Номер')}`) &&
-                    record.get('Модерация') &&
-                    areas.includes(record.get('Район')) &&
-                    beds.includes(record.get('Количество спален')) &&
-                    price >= record.get('Цена долларов в месяц')
-                if (minPrice != null) {
-                  filtered = filtered &&
-                    minPrice <= record.get('Цена долларов в месяц');
-                }
-                return filtered;
-            });
+            return records;
         } catch (exception) {
             console.error(`issue detected ...\n${exception}`);
             return [];
